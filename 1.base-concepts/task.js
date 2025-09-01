489d3ad6cbf4213be9b3fca5edff772ad02276e1
Andrey Kharlanov <kharlanov.horn@gmail.com>
2025-09-01 13:58:25 +0300

    Решение задания 1 и 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = Math.pow(b, 2) - 4 * a * c;
+
+  if (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+  }
+  if (d === 0) {
+    arr.push(-b / (2 * a));
+  } else {
+    arr;
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+  let loanBody = amount - contribution;
+  let monthPercent = percent / (12 * 100);
+
+  let monthPayment = loanBody * (monthPercent + (monthPercent / (Math.pow((1 + monthPercent), countMonths) - 1)));
+  let result = monthPayment * countMonths;
+  return +result.toFixed(2);
 }
