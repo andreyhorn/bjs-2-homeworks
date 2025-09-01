c5e6e9de692669428845f88e9f41a57bce785271
Andrey Kharlanov <kharlanov.horn@gmail.com>
2025-09-01 15:17:40 +0300

    Добавлено решение задания

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,31 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.hasOwnProperty('marks')) {
+        this.marks.push(...marks);
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.hasOwnProperty('marks') && this.marks.length !== 0) {
+        let result = this.marks.reduce((acc, mark, i, arr) => acc + mark / arr.length, 0);
+        return result;
+    } else {
+        return 0;
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    this.excluded = reason;
+    delete this.subject;
+    delete this.marks;
 }
