94ecd68fc79d54d360a50601e097d8752beb650e
Andrey Kharlanov <kharlanov.horn@gmail.com>
2025-09-01 16:03:46 +0300

    Добавлено решение заданий

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,47 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    const cache = {};
+
+    return function (...args) {
+        const hash = md5(args);
+        if (hash in cache) {
+            console.log("Из кеша: " + cache[hash]);
+            return "Из кеша: " + cache[hash];
+        }
+        const result = func(...args);
+        cache[hash] = result;
+        if (Object.keys(cache).length > 5) {
+            const firstKey = Object.keys(cache)[0];
+            delete cache[firstKey];
+        }
+
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;
+    }
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+    let timeoutId = null;
+    wrapper.count = 0;
+    wrapper.allCount = 0
+
+    function wrapper(...args) {
+        wrapper.allCount++;
+
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+
+        if (!timeoutId) {
+            func(...args);
+            wrapper.count++;
+        }
+        timeoutId = setTimeout(() => {
+            func(...args);
+            wrapper.count++;
+        }, delay);
+    }
+
+    return wrapper;
 }
