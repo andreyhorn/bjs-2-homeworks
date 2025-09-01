49d740190ef736d69ca844d8aa58eab80b542539
Andrey Kharlanov <kharlanov.horn@gmail.com>
2025-09-01 15:40:38 +0300

    Добавлено решение заданий

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,58 @@
-﻿
+﻿// Форматтер
+
+function parseCount(str) {
+   const num = Number.parseFloat(str);
+   if ( isNaN(num) === true) {
+      throw new Error("Невалидное значение");
+   }
+   return num;
+}
+
+function validateCount(str) {
+   try {
+      return parseCount(str);
+   } catch (error)  {
+      return error;
+   }
+}
+
+// Треугольник
+
+class Triangle {
+   constructor(a, b, c) {
+      if ((a + b <= c) || (a + c <= b) || (b + c <= a)) {
+         throw new Error("Треугольник с такими сторонами не существует");
+      }
+
+      this.a = a;
+      this.b = b;
+      this.c = c;
+   }
+
+   get perimeter() {
+      return this.a + this.b + this.c;
+   }
+
+   get area() {
+      const p = this.perimeter / 2;
+      const square = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+      return +square.toFixed(3);
+   }
+
+}
+
+function getTriangle(a, b, c) {
+   try {
+      return new Triangle(a, b, c);
+   } catch(error) {
+      const obj = {
+         get perimeter() {
+            return "Ошибка! Треугольник не существует";
+         },
+         get area() {
+            return "Ошибка! Треугольник не существует";
+         }
+      }
+      return obj;
+   }
+}
